0292560753a3edc4061c4107f77f1ef553765c66
Tianming Qiu <qiu@fortiss.org>
2022-03-23 10:42:43 +0100

    feat: patch feature cosine
    24x24 patch cosine similarities don't make any sense

--- demo/demo_scatter.py
+++ demo/demo_scatter.py
@@ -5,50 +5,71 @@ import os
 from matplotlib import pyplot as plt
 from kymatio.torch import Scattering2D
 
-torch.manual_seed(42)
-torch.cuda.manual_seed(42)
+# torch.manual_seed(42)
+# torch.cuda.manual_seed(42)
 
-DOWNLOAD_PATH = './input/dataset'
-SAVE_FOLDER = './checkpoint'
-BATCH_SIZE_TRAIN = 1
-BATCH_SIZE_TEST = 1
+DOWNLOAD_PATH = '../input/dataset'
+# SAVE_FOLDER = '../checkpoint'
+# BATCH_SIZE_TRAIN = 1
+# BATCH_SIZE_TEST = 1
 
-transform_stl10 = transforms.Compose([
-    transforms.ToTensor(),
-])
 
-train_set = torchvision.datasets.STL10(DOWNLOAD_PATH, split='train', download=True,
-                                       transform=transform_stl10)
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE_TRAIN, shuffle=True, pin_memory=True)
 
-def cosine_similarity(x,y):
+def cosine_similarity(x,y, head=None):
     x = x.reshape(-1) - x.mean()
     y = y.reshape(-1) - y.mean()
+    if head:
+        x = x[head[0]:head[1]]
+        y = y[head[0]:head[1]]
     return sum(x * y) / torch.norm(x) / torch.norm(y)
 
-for i, (data, target) in enumerate(train_loader):
-    image = data[0].cpu()
-
-    scatter2d = Scattering2D(J=1,L=4,shape=(8,8))
+def scatter_full_img(image, plot_feat=True):
     scatter2d_full = Scattering2D(J=1,L=4,shape=(96,96))
 
     # Inspect full image
     scatter_full = scatter2d_full(image.contiguous()).permute(1,2,3,0)
-    for i in range(scatter_full.shape[0]):
-        # Set large scale for high pass images for better visuals
-        img_scale = 10 if i > 0 else 1
-        plt.imshow(scatter_full[i] * img_scale)
-        plt.show()
+    if plot_feat:
+        for i in range(scatter_full.shape[0]):
+            # Set large scale for high pass images for better visuals
+            img_scale = 10 if i > 0 else 1
+            plt.imshow(scatter_full[i] * img_scale)
+            plt.show()
+    return scatter_full
 
 
+def scatter_patch(image, patch_size, p_coord, plot_feat=True):
+    scatter2d = Scattering2D(J=1,L=4,shape=(patch_size, patch_size))
     # Inspect image patch
-    patch1 = image[:,24:32,24:32]
+    p_x, p_y = p_coord[0], p_coord[1]
+    patch1 = image[:, p_x:p_x+patch_size, p_y:p_y+patch_size]
     scatter_patch = scatter2d(patch1.contiguous()).permute(1,2,3,0)
-    for i in range(scatter_patch.shape[0]):
-        # Set large scale for high pass images for better visuals
-        img_scale = 10 if i > 0 else 1
-        plt.imshow(scatter_patch[i] * img_scale)
-        plt.show()
+    if plot_feat:
+        for i in range(scatter_patch.shape[0]):
+            # Set large scale for high pass images for better visuals
+            img_scale = 10 if i > 0 else 1
+            plt.imshow(scatter_patch[i] * img_scale)
+            plt.show()
+    return scatter_patch
 
-    if i > -1:
-        break
+if __name__ == "__main__":
+    transform_stl10 = transforms.Compose([
+    transforms.ToTensor(),
+    ])
+
+    train_set = torchvision.datasets.STL10(DOWNLOAD_PATH, split='train', download=False,
+                                        transform=transform_stl10)
+    # train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE_TRAIN, shuffle=True, pin_memory=True)
+    # for i, (data, target) in enumerate(train_loader):
+    image = train_set[0][0]
+    p1 = scatter_patch(image, 24, (36, 36))
+    p2 = scatter_patch(image, 24, (42, 42))
+    p3 = scatter_patch(image, 24, (0, 10))
+    print(
+        cosine_similarity(p1, p2),
+        cosine_similarity(p1, p3),
+        cosine_similarity(p2, p3)
+    )
+
+    
+
+

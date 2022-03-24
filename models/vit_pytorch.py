1ad5c952f2b92f117adc75390f7c07405bba08be
Cheng Yan <yancheng0930@gmail.com>
2022-03-24 10:05:22 +0100

    Feat: scattering patch ViT model

--- models/vit_pytorch.py
+++ models/vit_pytorch.py
@@ -238,3 +238,53 @@ class ViT_with_conv_head(nn.Module):
         x = self.to_cls_token(x[:, 0])
         return self.mlp_head(x)
 '''
+
+class scatter_patch_ViT(nn.Module):
+    def __init__(self, *, scatter_angle, scatter_layer, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, channels = 3, dropout = 0., emb_dropout = 0.):
+        super().__init__()
+
+        self.scatter_angle = 8
+        self.scatter_layer = 3
+
+        self.patch_size = image_size/(2**(scatter_layer-1))
+        self.scatter = Scattering2D(J=self.scatter_layer,L=self.scatter_angle, shape=(patch_size,patch_size))
+
+        num_patches = scatter_angle ** (scatter_layer-1)
+        patch_dim = int(channels * patch_size ** 2 * (1 + self.scatter_angle)) 
+        
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
+        self.patch_to_embedding = nn.Linear(patch_dim, dim)
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.transformer = Transformer(dim, depth, heads, mlp_dim, dropout)
+
+        self.to_cls_token = nn.Identity()
+
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, mlp_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(mlp_dim, num_classes)
+        )
+
+    def forward(self, img, mask = None):
+        p = self.patch_size
+        scattering = self.scatter(img)
+        # x = rearrange(img, 'b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = p, p2 = p)
+        x = scattering.view(scattering.shape[0], scattering.shape[1], -1)
+        
+        x = self.patch_to_embedding(x)
+        b, n, _ = x.shape
+
+        cls_tokens = self.cls_token.expand(b, -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x += self.pos_embedding[:, :(n + 1)]
+        x = self.dropout(x)
+
+        x = self.transformer(x, mask)
+
+        x = self.to_cls_token(x[:, 0])
+        return self.mlp_head(x)

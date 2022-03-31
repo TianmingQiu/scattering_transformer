af845b244e8603ed1593f410d74317bda3f555d7
Tianming Qiu <qiu@fortiss.org>
2022-03-31 22:21:21 +0200

    feat: demo for scattering

--- demo/demo_scatter.py
+++ demo/demo_scatter.py
@@ -33,6 +33,7 @@ def scatter_full_img(image, plot_feat=True):
             # Set large scale for high pass images for better visuals
             img_scale = 10 if i > 0 else 1
             plt.imshow(scatter_full[i] * img_scale)
+            plt.axis('off')
             plt.show()
     return scatter_full
 
@@ -42,14 +43,21 @@ def scatter_patch(image, patch_size, p_coord, plot_feat=True):
     # Inspect image patch
     p_x, p_y = p_coord[0], p_coord[1]
     patch1 = image[:, p_x:p_x+patch_size, p_y:p_y+patch_size]
+    tensor2imgshow(patch1)
     scatter_patch = scatter2d(patch1.contiguous()).permute(1,2,3,0)
     if plot_feat:
         for i in range(scatter_patch.shape[0]):
             # Set large scale for high pass images for better visuals
             img_scale = 10 if i > 0 else 1
             plt.imshow(scatter_patch[i] * img_scale)
+            plt.axis('off')
             plt.show()
     return scatter_patch
+
+def tensor2imgshow(img_tensor):
+    plt.imshow(img_tensor.permute(1,2,0).detach().cpu())
+    plt.axis('off')
+    plt.show()
 
 if __name__ == "__main__":
     transform_stl10 = transforms.Compose([
@@ -60,14 +68,17 @@ if __name__ == "__main__":
                                         transform=transform_stl10)
     # train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE_TRAIN, shuffle=True, pin_memory=True)
     # for i, (data, target) in enumerate(train_loader):
-    image = train_set[0][0]
-    p1 = scatter_patch(image, 24, (36, 36))
-    p2 = scatter_patch(image, 24, (42, 42))
-    p3 = scatter_patch(image, 24, (0, 10))
+    image = train_set[1][0]
+    tensor2imgshow(image)
+    scatter_full_img(image, plot_feat=True)
+    p1 = scatter_patch(image, 32, (80, 40), )
+    p2 = scatter_patch(image, 32, (50, 30), )
+    p3 = scatter_patch(image, 32, (0, 0), )
     print(
         cosine_similarity(p1, p2),
         cosine_similarity(p1, p3),
-        cosine_similarity(p2, p3)
+        cosine_similarity(p2, p3),
+        cosine_similarity(torch.tensor([0.0,0.0, 1.0]), torch.tensor([1.0,0.0, 0.0]))
     )
 
     
